75c2b44d12428f48899ecd054e7d3acfc46b5fbc
Alma Ozoria <heisylior@hotmail.com>
2025-05-28 19:46:26 -0400

    Despendiacias listas

---
+++ app/src/main/java/edu/ucne/ap2_p1_almarosa/MainActivity.kt
@@ -0,0 +1,47 @@
+package edu.ucne.ap2_p1_almarosa
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import edu.ucne.ap2_p1_almarosa.ui.theme.Ap2_P1_AlmaRosaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Ap2_P1_AlmaRosaTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Ap2_P1_AlmaRosaTheme {
+        Greeting("Android")
+    }
+}
